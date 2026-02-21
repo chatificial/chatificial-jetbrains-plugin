74c13a820ba447aced1b9b3acc73a62a57238d44
Grig Alex <alexengrigdev@gmail.com>
2026-02-22 01:08:35 +0300

    Remove comments
    Signed-off-by: Grig Alex <alexengrigdev@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,13 +30,10 @@ repositories {
     }
 }
 
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
     intellijPlatform {
         intellijIdea("2025.2.4")
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
-
-        // Add plugin dependencies for compilation here:
     }
 }
 
@@ -53,7 +50,6 @@ intellijPlatform {
 }
 
 tasks {
-    // Set the JVM compatibility versions
     withType<JavaCompile> {
         sourceCompatibility = "21"
         targetCompatibility = "21"
